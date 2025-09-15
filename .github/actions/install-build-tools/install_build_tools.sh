d2a2b5ba9a77a92ca003bff3d722e4c90ea437b3
T-im4i-k <138623023+T-im4i-k@users.noreply.github.com>
2025-09-15 22:36:26 +0200

    Switch Package Manager Tool For install-build-tools Action - Switch: apt -> apt-get

--- .github/actions/install-build-tools/install_build_tools.sh
+++ .github/actions/install-build-tools/install_build_tools.sh
@@ -15,7 +15,7 @@ function print_boxed(){
 if which cmake &>/dev/null && cmake --version &>/dev/null; then
   printf "Found cmake installation:\n"
   print_boxed "$(cmake --version)"
-elif sudo apt install -y cmake &>/dev/null; then
+elif sudo apt-get install -y cmake &>/dev/null; then
   printf "Installed cmake:\n"
   print_boxed "$(cmake --version)"
 else
@@ -26,7 +26,7 @@ fi
 if which ninja &>/dev/null && ninja --version &>/dev/null; then
   printf "Found ninja installation:\n"
   print_boxed "$(ninja --version)"
-elif sudo apt install -y ninja-build &>/dev/null; then
+elif sudo apt-get install -y ninja-build &>/dev/null; then
   printf "Installed ninja:\n"
   print_boxed "$(ninja --version)"
 else
@@ -37,7 +37,7 @@ fi
 if which clang &>/dev/null && clang --version &>/dev/null; then
   printf "Found clang installation:\n"
   print_boxed "$(clang --version)"
-elif sudo apt install -y clang &>/dev/null; then
+elif sudo apt-get install -y clang &>/dev/null; then
   printf "Installed clang:\n"
   print_boxed "$(clang --version)"
 else
