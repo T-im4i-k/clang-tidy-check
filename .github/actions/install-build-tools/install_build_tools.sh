7faf83c3875923fb36ff30b9e13193041fa51e4b
T-im4i-k <138623023+T-im4i-k@users.noreply.github.com>
2025-09-15 22:56:13 +0200

    Improve install-build-tools Action Logging

--- .github/actions/install-build-tools/install_build_tools.sh
+++ .github/actions/install-build-tools/install_build_tools.sh
@@ -1,41 +1,46 @@
-function print_delim(){
-  printf -- "---\n"
-}
-
-function print_boxed(){
-  if [ "$#" -ne 1 ]; then
-    printf "Error: print_boxed() expects 1 argument, but %s were provided\n" "$#"
-  fi
-
-  print_delim
-  printf "%s\n" "$1"
-  print_delim
+function print_delim() {
+	printf -- "---\n"
 }
 
 printf "Installing required packages...\n"
+print_delim
 if which cmake &>/dev/null && cmake --version &>/dev/null; then
-  print_boxed "Found cmake installation:\n$(cmake --version)"
+	printf "Found cmake installation:\n"
+	printf "%s\n" "$(cmake --version)"
+	print_delim
 elif sudo apt-get install -y cmake &>/dev/null; then
-  print_boxed "Installed cmake:\n$(cmake --version)"
+	printf "Installed cmake:\n"
+	printf "%s\n" "$(cmake --version)"
+	print_delim
 else
-  printf "Error: cmake installation failed.\n"
-  exit 1
+	printf "Error: cmake installation failed.\n"
+	exit 1
 fi
 
 if which ninja &>/dev/null && ninja --version &>/dev/null; then
-  print_boxed "Found ninja installation:\n$(ninja --version)"
+	printf "Found ninja installation:\n"
+	printf "%s\n" "$(ninja --version)"
+	print_delim
 elif sudo apt-get install -y ninja-build &>/dev/null; then
-  print_boxed "Installed ninja:\n$(ninja --version)"
+	printf "Installed ninja:\n"
+	printf "%s\n" "$(ninja --version)"
+	print_delim
 else
-  printf "Error: ninja installation failed.\n"
-  exit 1
+	printf "Error: ninja installation failed.\n"
+	exit 1
 fi
 
 if which clang &>/dev/null && clang --version &>/dev/null; then
-  print_boxed "Found clang installation:\n$(clang --version)"
+	printf "Found clang installation:\n"
+	printf "%s\n" "$(clang --version)"
+	print_delim
 elif sudo apt-get install -y clang &>/dev/null; then
-  print_boxed "Installed clang:\n$(clang --version)"
+	printf "Installed clang:\n"
+	printf "%s\n" "$(clang --version)"
+	print_delim
 else
-  printf "Error: clang installation failed.\n"
-  exit 1
+	printf "Error: clang installation failed.\n"
+	exit 1
 fi
+
+printf "### Successfully installed required packages ###\n"
